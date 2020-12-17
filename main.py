547e6f557ca53c99b3d54e5210dffd46c239e06e
yusuketosa <y.tosa.kyoto@gmail.com>
2020-12-17 21:15:25 +0900

    underflow修正

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 import numpy as np
 from PIL import Image
@@ -8,58 +9,66 @@ class EMSolver():
         self.M = M
         self.sigma = sigma
 
-    def calc_posterior(self, mu, i):
-        def gaussian(x, mu=mu, i=i):
-            return np.exp(-1/(2 * self.sigma**2)*np.linalg.norm(x - mu[i])**2)
+    def _calc_posterior(self, i):
+        def quadra(norm):
+            return -norm/(2 * self.sigma**2 + 1e-10)
 
-        def wrapper(x, mu=mu, i=i):
-            return gaussian(x, mu, i) / np.sum([gaussian(x, mu, j) for j in range(self.M)])
+        def wrapper(x, i=i):
+            norms = [np.linalg.norm(x - self.mu[j])**2 for j in range(self.M)]
+            norm = norms[i]
+            min_norm = np.min(norms)
+            return np.exp(quadra(norm) - quadra(min_norm) - np.log(np.sum(np.exp(quadra(norms - min_norm)))))
         return wrapper
 
-    def calc_expectation(self, mu, i):
-        def gaussian(x, mu=mu, i=i):
-            return np.exp(-1/(2 * self.sigma**2)*np.linalg.norm(x - mu[i])**2)
+    def _calc_expectation(self, i):
+        def quadra(norm):
+            return -norm/(2 * self.sigma**2 + 1e-10)
 
-        def wrapper(x, mu=mu, i=i):
-            return x * (gaussian(x, mu, i) / np.sum([gaussian(x, mu, j) for j in range(self.M)]))
+        def wrapper(x, i=i):
+            norms = [np.linalg.norm(x - self.mu[j])**2 for j in range(self.M)]
+            norm = norms[i]
+            min_norm = np.min(norms)
+            return x * (np.exp(quadra(norm) - quadra(min_norm) - np.log(np.sum(np.exp(quadra(norms - min_norm))))))
         return wrapper
 
     def load(self, csv_data):
         data = np.loadtxt(csv_data, delimiter=",", skiprows=1)/256
-        init_mu = data[np.random.choice(data.shape[0], self.M, replace=False), :]
-        return data, init_mu
+        self.mu = data[np.random.choice(data.shape[0], self.M, replace=False), :]
+        return data
 
-    def e_step(self, data, mu):
+    def e_step(self, data):
         stats = []
         for i in range(self.M):
-            partition = np.mean(np.apply_along_axis(self.calc_posterior(mu=mu, i=i), 1, data))
-            expectation = np.mean(np.apply_along_axis(self.calc_expectation(mu=mu, i=i), 1, data), axis=0)
+            partition = np.mean(np.apply_along_axis(self._calc_posterior(i=i), 1, data))
+            expectation = np.mean(np.apply_along_axis(self._calc_expectation(i=i), 1, data), axis=0)
             stats.append((expectation, partition))
         return stats
 
     def m_step(self, stats):
-        mu = np.array([e/p for e, p in stats])
-        return mu
+        self.mu = np.array([e/p for e, p in stats])
 
 
 def main():
-    M = 3
-    sigma = 1.0
-    solver = EMSolver(M, sigma)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-M", type=int, default=3, help="Number of categories")
+    parser.add_argument("-S", "--sigma", type=float, default=1.0, help="Variance of normal distribution")
+    args = parser.parse_args()
+
+    solver = EMSolver(args.M, args.sigma)
     print('loading csv')
-    data, mu = solver.load("mnist_em.csv")
-    delta_mu = np.full(M, np.inf)
+    data = solver.load("datasets/mnist_em.csv")
+    delta_mu = np.full(solver.M, np.inf)
     while np.any(delta_mu > 0.01):
-        mu_old = mu
-        stats = solver.e_step(data, mu)
-        mu = solver.m_step(stats)
-        delta_mu = np.sum(np.square(mu - mu_old), axis=1)
+        mu_old = solver.mu
+        stats = solver.e_step(data)
+        solver.m_step(stats)
+        delta_mu = np.sum(np.square(solver.mu - mu_old), axis=1)
         print(delta_mu)
     print("finish")
 
     label = "abcdefghi"
-    if M > len(label):
-        label += ''.join([str(i) for i in range(M - len(label))])
+    if solver.M > len(label):
+        label += ''.join([str(i) for i in range(solver.M - len(label))])
 
     output_dir = "output_fig"
 
@@ -67,7 +76,7 @@ def main():
     for f in files:
         os.remove(os.path.join(output_dir, f))
 
-    for i, item in enumerate(mu):
+    for i, item in enumerate(solver.mu):
         item = np.round(item*256).astype(np.uint8)
         item = item.reshape((28, 28))
         img = Image.fromarray(item, 'L')
